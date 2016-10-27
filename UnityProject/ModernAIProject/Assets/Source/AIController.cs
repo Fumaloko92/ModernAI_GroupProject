9879295a278c5ba5a419d1506870b3077644e1a0
Luca Salfi <lucasalfi2@gmail.com>
2016-10-27 11:42:59 +0200

    Code base added

---
+++ UnityProject/ModernAIProject/Assets/Source/AIController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIController : MonoBehaviour {
+    protected List<Resource> collectedResources;
+    protected Behavior currentBehavior;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
