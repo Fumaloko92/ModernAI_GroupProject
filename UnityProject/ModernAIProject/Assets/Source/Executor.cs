17967a246f954470728c663afd798357de1cf2f6
Luca Salfi <lucasalfi2@gmail.com>
2016-10-27 11:47:23 +0200

    Code base added (#1)
    Code base added
    Class diagram updated

---
+++ UnityProject/ModernAIProject/Assets/Source/Executor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Executor : MonoBehaviour {
+    private int populationCount;
+    private int maxGenerations;
+    private int generation;
+    private List<AIController> population;
+    private World currentWorld;
+    private State currentState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GenerateWorld()
+    {
+
+    }
+
+    public void InitializePopulation()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void Evaluate()
+    {
+
+    }
+}
---
+++ UnityProject/ModernAIProject/Assets/Source/World.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour {
+    List<GatheringPlace> gatheringPlaces;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
