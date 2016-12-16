d5534a8a5ef2f9c0f19bc8b172c9bb87bb4e8dc3
Muhsin <muhsinkaymak@hotmail.com>
2016-12-16 11:26:30 +0100

    high resource experiment

--- UnityProject/ModernAIProject/Assets/Source/World.cs
+++ UnityProject/ModernAIProject/Assets/Source/World.cs
@@ -20,7 +20,7 @@ public class World : MonoBehaviour {
         terrain = GameObject.FindGameObjectWithTag("Terrain");
         terrainSize = new Vector2(terrain.GetComponent<Terrain>().terrainData.size.x, terrain.GetComponent<Terrain>().terrainData.size.z);
 
-        resourceCount = StaticRandom.Rand(1, 10);
+        resourceCount = 1000;// StaticRandom.Rand(50, 1000);
         resourcePositions = new List<Vector3>();
         while (resourceCount > 0)
         {
